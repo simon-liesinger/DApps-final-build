7c148f93a7284e78bf02cc9bf0faf07bd904600f
Simon Liesinger <36867918+simon-liesinger@users.noreply.github.com>
2026-09-25 13:00:08 +1000

    merge conflict test 2 part A

--- Assets/bullet.cs
+++ Assets/bullet.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    int sum;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sum = 3+4;
     }
 
     // Update is called once per frame
