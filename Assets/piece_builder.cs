86250d2477f7d1cc2cb74a36acd5a8cbfa2efe11
Simon Liesinger <36867918+simon-liesinger@users.noreply.github.com>
2026-09-25 13:49:35 +1000

    fix and make puzzle pieces?? maybe?

--- Assets/piece_builder.cs
+++ Assets/piece_builder.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 
 public class piece_builder : MonoBehaviour
 {
     public enum PieceType {T, L, R, O, I, S, Z};
     public PieceType variant;
-    public Transform[] parts = new Transform[5];
-    public Vector2Int pentominoPosition;
-    public bool pentomino;
+    public List<Transform> parts = new List<Transform>{};
+    public int extraPieces = 0;
+    public List<Vector2Int> extraPositions = new List<Vector2Int>{};
+    public GameObject piecePrefab;
     void OnValidate()
     {
-        parts[4].gameObject.SetActive(pentomino);
-        parts[4].localPosition = (Vector2)pentominoPosition;
+        if (extraPieces < 0)
+        {
+            extraPieces = 0; // can't have less than 0
+        }
+        if (extraPieces+4 > parts.Count)
+        {
+            while (extraPieces+4 > parts.Count)
+            {
+                parts.Add(Instantiate(piecePrefab, transform).transform); // make some new ones to fill the count
+                extraPositions.Add(new Vector2Int(0,0));
+            }
+        } else if (extraPieces+4 < parts.Count)
+        {
+            while (extraPieces+4 < parts.Count)
+            {
+                DestroyImmediate(parts[parts.Count-1].gameObject);
+                parts.RemoveAt(parts.Count-1);
+                extraPositions.RemoveAt(extraPositions.Count-1);
+            }
+        }
         switch (variant)
         {
             case PieceType.T:
@@ -50,6 +70,10 @@ public class piece_builder : MonoBehaviour
                 parts[2].localPosition = new Vector3( 0,  1, 0);
                 parts[3].localPosition = new Vector3(-1,  1, 0);
                 break;
+        }
+        for (int i = 0; i < extraPieces; i++)
+        {
+            parts[i+4].localPosition = (Vector2)extraPositions[i];
         }
     }
     // Start is called before the first frame update
